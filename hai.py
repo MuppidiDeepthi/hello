6153ad3fdedeecf0990ef121e7387f9e3a0167bc
MuppidiDeepthi <124231304+MuppidiDeepthi@users.noreply.github.com>
2023-12-29 11:57:39 +0530

    Update hai.py

--- hai.py
+++ hai.py
@@ -5,7 +5,7 @@ st.write("<h1 style='color:blue;'>Good morning</h1>",unsafe_allow_html=True)
 
 st.file_uploader("upload file")
 
-st.image("https://animaders.com/wp-content/uploads/2021/10/Tom-and-Jerry-Cartoon-A-Short-History-of-Americas-Favorite-Cartoon-Team-animader.jpg")
+st.image("https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*")
 st.video("https://youtu.be/AGBjI0x9VbM?si=M34MEMScBz7EjkKe")
 img = st.file_uploader("Upload image")
 st.image(img)
