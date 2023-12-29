2aa6c839ee826689d5b779acede3b252038644f1
MuppidiDeepthi <124231304+MuppidiDeepthi@users.noreply.github.com>
2023-12-29 11:59:41 +0530

    Update hai.py

--- hai.py
+++ hai.py
@@ -6,6 +6,6 @@ st.write("<h1 style='color:blue;'>Good morning</h1>",unsafe_allow_html=True)
 st.file_uploader("upload file")
 
 st.image("https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*")
-st.video("https://www.youtube.com/shorts/KIjNp98bxIM?feature=share")
+st.video("https://youtu.be/CRvMD1gn6Ko?si=lMRqPuUfh_qGL74n")
 img = st.file_uploader("Upload image")
 st.image(img)
